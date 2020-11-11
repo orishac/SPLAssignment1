1cd5477acf8e249ccaa4e544e647ce65ebfe25a9
spl211 <orishac@post.bgu.ac.il>
2020-11-11 16:10:48 +0200

    added implementation to something

--- src/Graph.cpp
+++ src/Graph.cpp
@@ -5,13 +5,15 @@
 Graph::Graph(std::vector<std::vector<int>> matrix) : edges (matrix) {
 };
 
-void infectNode(int nodeInd) {
+void Graph::infectNode(int nodeInd) {
     Virus* v =  new Virus(nodeInd);
 
 }
-bool isInfected(int nodeInd){
-
-}
+bool Graph::isInfected(int nodeInd){
+    if (infection[nodeInd] == true)
+        return true;
+    return false;
+};
 
 int Graph::getNonInfectedNeighbor(int NodeInd) {
     for (int i = 0; i<edges[NodeInd].size(); i++) {
